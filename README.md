c5c4f3ad66ac949d76890d2026c478cbf5bbef06
Minwoo Lee <minwoolee@snu.ac.kr>
2020-06-18 15:11:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# InfoGraph - PyTorch (PyTorch Geometric) Implementation
+
+This is an unoffical re-implementation of Sun et al.'s **InfoGraph: Unsupervised and Semi-supervised Graph-Level Representation Learning via Mutual Information Maximization** (ICLR 2020).
+
+Official implementation is available [here](https://github.com/fanyun-sun/InfoGraph).
+
+This implementation is basically a cleaned-up and commented version of the official implementation.
+
+## Requirements
+ - python 3.7
+ - pytorch 1.5.0
+ - pytorch-geometric 1.5.0
+
+
+## Run unsupervised Learning
+`python unsupervised.py`
---
+++ unsupervised.py
@@ -0,0 +1,201 @@
+import math
+import argparse
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+from torch_geometric.data import DataLoader
+from torch_geometric.datasets import TUDataset
+from torch_geometric.nn import GINConv, global_add_pool
+
+DATA_DIR = Path("./data/")
+
+
+class GINEncoder(nn.Module):
+    """Graph/patch representation encoder using GIN as base GNN layer."""
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super().__init__()
+
+        self.convs = nn.ModuleList()
+        self.bns = nn.ModuleList()
+
+        for i in range(num_layers):
+            self.convs.append(
+                GINConv(
+                    nn.Sequential(
+                        nn.Linear(input_dim if i == 0 else hidden_dim, hidden_dim),
+                        nn.ReLU(),
+                        nn.Linear(hidden_dim, hidden_dim)
+                    ),
+                )
+            )
+            self.bns.append(
+                nn.BatchNorm1d(hidden_dim)
+            )
+
+    def forward(self, graph_data):
+        x = graph_data.x
+        xs = []
+        for conv, bn in zip(self.convs, self.bns):
+            x = F.relu(conv(x, graph_data.edge_index))
+            x = bn(x)
+            xs.append(x)
+
+        x_patches = torch.cat(xs, dim=1)
+        x_global = global_add_pool(x_patches, graph_data.batch)
+        return x_global, x_patches
+
+
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU(),
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU(),
+            nn.Linear(input_dim, input_dim),
+            nn.ReLU()
+        )
+        self.linear_shortcut = nn.Linear(input_dim, input_dim)
+
+    def forward(self, x):
+        return self.block(x) + self.linear_shortcut(x)
+
+
+class GlobalLocalDiscriminator(nn.Module):
+    """Discriminator model for comparing graph/patch representation pairs"""
+    def __init__(self, embed_dim):
+        super().__init__()
+        self.global_encoder = FeedForwardNetwork(embed_dim)
+        self.patch_encoder = FeedForwardNetwork(embed_dim)
+
+    def forward(self, x_global, x_patch, batch):
+        h_global = self.global_encoder(x_global)
+        h_patch = self.patch_encoder(x_patch)
+        score_matrix = torch.mm(h_global, h_patch.T)
+
+        # compute real-fake label mask
+        mask = torch.zeros_like(score_matrix, dtype=torch.bool)
+        mask[batch, torch.arange(len(batch))] = 1
+
+        real_scores = score_matrix[mask]
+        fake_scores = score_matrix[~mask]
+
+        loss = - self.compute_mutual_information(real_scores, fake_scores)
+
+        return loss
+
+    @staticmethod
+    def compute_mutual_information(real_scores, fake_scores):
+        """Compute mutual information using Jenson-shannon divergence between real pairs and fake pairs"""
+
+        E_positive = (math.log(2) - F.softplus(- real_scores)).mean()
+        E_negative = (- math.log(2) + fake_scores + F.softplus(- fake_scores)).mean()
+
+        return E_positive - E_negative
+
+
+class InfoGraph(nn.Module):
+    """Unsupervised graph representation learning framewark"""
+    def __init__(self, input_dim, encoder_hidden_dim, num_encoder_layers, alpha=0.5, beta=1., gamma=.1):
+        super().__init__()
+        self.alpha = alpha
+        self.beta = beta
+        self.gnn_encoder = GINEncoder(input_dim, encoder_hidden_dim, num_encoder_layers)
+        self.discriminator = GlobalLocalDiscriminator(encoder_hidden_dim * num_encoder_layers)
+
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight.data)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias.data)
+
+    def forward(self, graph_data):
+        h_global, h_patches = self.gnn_encoder(graph_data)
+        loss = self.discriminator(h_global, h_patches, graph_data.batch)
+        return loss
+
+    def encode_graph(self, graph_data):
+        """Get graph representation for downstream tasks"""
+        h_global, _ = self.gnn_encoder(graph_data)
+        return h_global
+
+
+def train(model, dataloader, optimizer, device):
+    model.train()
+    total_loss = 0
+    for data in dataloader:
+        if data.x is None:
+            data.x = torch.ones(data.batch.shape[0])
+        data = data.to(device)
+        optimizer.zero_grad()
+        loss = model(data)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item() * data.num_graphs
+    return total_loss / len(dataloader)
+
+
+def evaluate(model, dataloader, device):
+    model.eval()
+    total_loss = 0
+    with torch.no_grad():
+        for data in dataloader:
+            if data.x is None:
+                data.x = torch.ones(data.batch.shape[0])
+            data = data.to(device)
+            loss = model(data)
+            total_loss += loss.item() * data.num_graphs
+    print(total_loss / len(dataloader))
+
+
+def main():
+    # --------------------- PARSE ARGS -----------------------
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--dataset", choices=['MUTAG'], default='MUTAG')
+    parser.add_argument("--batch-size", type=int, default=128)
+    parser.add_argument("--lr", type=float, default=0.01)
+    parser.add_argument("--num-epoch", type=int, default=20)
+    parser.add_argument("--encoder-hidden-dim", type=int, default=32)
+    parser.add_argument("--num-encoder-layers", type=int, default=3)
+
+    args = parser.parse_args()
+
+    print("- Args ----------------------")
+    for k, v in vars(args).items():
+        print(" - {}={}".format(k, v))
+    print("-----------------------------")
+
+    # --------------------- LOAD DATASET ---------------------
+    print("Loading dataset...")
+    dataset = TUDataset(DATA_DIR / args.dataset, name=args.dataset)
+    try:
+        dataset_num_features = dataset.num_features
+    except:
+        dataset_num_features = 1
+
+    dataloader = DataLoader(dataset, args.batch_size, shuffle=True)
+
+    # --------------------- TRAIN MODEL ----------------------
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = InfoGraph(dataset_num_features, args.encoder_hidden_dim, args.num_encoder_layers).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+
+    print("Starting training...")
+
+    for epoch in range(1, args.num_epoch+1):
+        train_loss = train(model, dataloader, optimizer, device)
+        print("| Epoch: {:3} | Unsupervised Loss: {:10.4f} |".format(epoch, train_loss))
+
+    print("Training finished!")
+
+
+if __name__ == "__main__":
+    main()
